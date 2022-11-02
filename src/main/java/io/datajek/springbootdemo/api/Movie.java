d34cc2834682a7d93982fdbb9d2a532bea139118
KNAndrikov18 <KNAndrikov18@codingburgas.bg>
2022-11-02 15:34:02 +0200

    Movie

---
+++ src/main/java/io/datajek/springbootdemo/api/Movie.java
@@ -0,0 +1,34 @@
+package io.datajek.springbootdemo.api;
+
+public class Movie {
+    int id;
+    String name;
+    double rating;
+
+    public Movie(){
+
+    }
+    public Movie(int id, String name, double rating){
+        super();
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String toString()
+    {
+        return "Movie [id = " + id + ", name=" + name + ", rating=" + rating + "]";
+    }
+}
